3e6f61a881f9646841cd1d0daac3592527a83b73
andrecosta8 <andrecosta8@gmail.com>
2022-12-28 18:34:07 +0000

    basic navigation tools and layouts

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from "../../containers/Home/Home";
+import Register from "../../containers/Register/Register";
+import Login from "../../containers/Login/Login";
+import Profile from "../../containers/Profile/Profile";
+import Products from "../../containers/Products/Products";
+import Detail from "../../containers/Detail/Detail";
 
 const Body = () => {
   return (
     <>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/products" element={<Products/>} />
+      <Route path="/detail" element={<Detail />} />
     </Routes>
   </>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Header.css'
 
 const Header = () => {
+  let adminToken = true;
+  let navigate = useNavigate();
   return (
-    <div>Header</div>
+    <div className='headerDesign'>
+    <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/")},250)}>Home</div>
+    <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/profile")},250)}>Profile</div>
+    <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/products")},250)}>Products</div>
+    <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/login")},250)}>Login</div>
+    <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/register")},250)}>Register</div>
+    {adminToken === true ? <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/admin")},250)}>Admin</div> : null}
+    
+    
+ </div>
   )
 }
 
